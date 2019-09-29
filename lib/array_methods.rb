11b8ff81c84097423a125e39c666903d64f9bc69
Rose Seraphin <roseseraphin@yahoo.com>
2019-09-29 00:58:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,9 +2,8 @@ def find_element_index(array, value_to_find)
 array.length.times do |counter|
   if array[counter] == value_to_find
     return counter
-  end 
 else 
-  return nil 
+  nil 
  end
 
 
