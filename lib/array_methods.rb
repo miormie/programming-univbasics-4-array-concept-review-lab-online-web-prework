24b41922b4a91887c0faa2b2b376fca4b0a8bc07
Rose Seraphin <roseseraphin@yahoo.com>
2019-09-29 01:34:37 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,48 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+array.length.times do |counter|
+  if array[counter] == value_to_find
+   return counter
+end 
+   
+ end
+nil
 end
+
+# array.length.times do |counter|
+#   if array[counter] == value_to_find
+#     return counter
+      # 2 parameters, array of integers and a value to find = return index of value that was passed
+  
+  # needs a loop for the given array and compare each value of array to the value given ********
+
 
 def find_max_value(array)
-  # Add your solution here
+  array.max 
+  
 end
 
+# find max value (.max)
+
+
+
+
+
+
+
 def find_min_value(array)
-  # Add your solution here
+  array.min 
 end
+
+
+
+
+# Looping through an array to find the position of a value
+# Finding a maximum value in an array
+# Finding a minimum value in an array
+
+# all arrays are sets of positive integers
+# methods and parameter names are provided for me 
+
+# needs to pass every array tests before moving on to next lesson 
+
+#
